207d60d55577cf882051fa6d9ea88f3639f375fb
SimenHus <simen_vh@live.no>
2024-04-13 13:05:30 +0200

    Readied script for self test

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-import cv2
+from dt_apriltags import Detector
+import numpy as np
 from os import path
+import cv2
+import yaml
 
 
-# Define pathing
-basePath = path.dirname(path.relpath(__file__))
-dataPath = path.join(basePath, 'data')
-imagePath = path.join(dataPath, 'images')
-aprilTagsPath = path.join(dataPath, 'april-tags')
+IMAGE_FOLDER = path.join(path.curdir, 'images')
+APRILTAGS = path.join(path.curdir, 'apriltags', 'tag36h11')
+PARAMS_FOLDER = path.join(path.curdir, 'params')
 
 
-image = cv2.imread(path.join(imagePath, 'image0000.jpg'))
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+with open('parameters.yaml', 'r') as f:
+    parameters = yaml.safe_load(f)
 
-cv2.imshow('', gray)
-cv2.waitKey(0)
+apriltagParams = parameters['apriltags']
+cameraParams = parameters['camera']
+
+at_detector = Detector(families=apriltagParams['family'],
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+imgName = 'test_image_multiple_01.png'
+colorImg = cv2.imread(path.join(IMAGE_FOLDER, imgName))
+grayImg = cv2.cvtColor(colorImg, cv2.COLOR_RGB2GRAY)
+intrinsicMatrix = np.array(cameraParams['K']).reshape((3,3))
+cameraCalibrationParams = (intrinsicMatrix[0,0], intrinsicMatrix[1,1],
+                           intrinsicMatrix[0,2], intrinsicMatrix[1,2])
+
+tags = at_detector.detect(grayImg, estimate_tag_pose=False, camera_params=cameraCalibrationParams)
+print(tags)
+
+for tag in tags:
+    for idx in range(len(tag.corners)):
+        cv2.line(colorImg, tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
+
+    cv2.putText(colorImg, str(tag.tag_id),
+                org=(tag.corners[0, 0].astype(int)+10,tag.corners[0, 1].astype(int)+10),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.8,
+                color=(0, 0, 255))
+
+cv2.imshow('Detected tags', colorImg)
+
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
